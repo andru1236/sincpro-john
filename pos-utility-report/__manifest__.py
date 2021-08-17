bbfffec8ca426dbbe797678761de6fa5559aa831
Yañez Villa Moises Edson <yanezvilla.edson@gmail.com>
2021-08-17 14:08:10 -0400

    Add the report_sale_details module

--- pos-utility-report/__manifest__.py
+++ pos-utility-report/__manifest__.py
@@ -1,6 +1,9 @@
 {
     'name': 'Custom Utility Report',
     'description': "",
+    "depends": [
+        "base", "report.point_of_sale",
+    ],
     'data': [
         'views/custom_report_sale_details.xml'
     ]
